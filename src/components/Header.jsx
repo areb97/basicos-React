e2ad958975a2e65399f8d753fbbe40085d8b7cc6
Beatriz Arevalo <arebeatriz97@gmail.com>
2020-03-22 21:13:45 -0600

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(){
+
+    return(   //Contiene todo lo que se ve en la pantalla
+
+        <h1>Desde el header</h1>
+
+    )
+}
+
+export default Header;
